4ee768ec680da773bbae52beaf96324b93fa5334
samraj-dev <51845296+samraj-dev@users.noreply.github.com>
2024-07-27 18:27:31 +0530

    feat(periods): implement sorting (#2)

--- src/entities/periods/api/get-periods.js
+++ src/entities/periods/api/get-periods.js
@@ -1,10 +1,10 @@
 import pb from "@/shared/lib/pocketbase";
 
-export const getAllPeriods = async () => {
+export const getAllPeriods = async (sort = "name") => {
   const result = await pb
     .collection("periods")
     .getFullList(200 /* batch size */, {
-      sort: "-created",
+      sort: sort,
     });
 
   return result;
